309c213bbe06e4a928a9cb957f8e51bb97e92de8
tgutmann87 <31452444+tgutmann87@users.noreply.github.com>
2021-12-13 17:01:35 -0800

    Cleaned up a bit

--- Log4J_Version_Check.py
+++ Log4J_Version_Check.py
@@ -5,21 +5,22 @@ from zipfile import ZipFile
 import configparser
 
 jarFile = sys.argv[1]
-zipper = ZipFile(jarFile, "r")
-#print(zipper.getinfo("META-INF/MANIFEST.MF"))
-zipper.extract("META-INF/MANIFEST.MF")
+if os.path.isfile(jarFile):
+	zipper = ZipFile(jarFile, "r")
+	zipper.extract("META-INF/MANIFEST.MF")
 
-file = open("META-INF/MANIFEST.MF", "r")
-temp = file.readlines()
-temp.insert(0, "[Deafult]\n")
-file.close()
+	file = open("META-INF/MANIFEST.MF", "r")
+	temp = file.readlines()
+	temp.insert(0, "[Deafult]\n")
+	file.close()
 
-file = open("tempManifest.txt", "wt")
-for line in temp:
-	file.write(line)
-file.close()
+	file = open("META-INF/MANIFEST.MF", "wt")
+	for line in temp:
+		file.write(line)
+	file.close()
 
-manifestParser = configparser.ConfigParser(delimiters=':',strict=False)
-manifestParser.read_file(open("tempManifest.txt", "rt"))
-#print(manifestParser.sections())
-print(manifestParser.get("Deafult","Implementation-Version"))
+	manifestParser = configparser.ConfigParser(delimiters=':',strict=False)
+	manifestParser.read_file(open("META-INF/MANIFEST.MF", "rt"))
+	print("Log4J Version is: " + manifestParser.get("Deafult","Implementation-Version"))
+else:
+	print("File Doesn't Exist!")
