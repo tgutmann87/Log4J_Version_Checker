4873bb539ff8612f5a8f9607c25eab0433886723
Tim <tgutmann87@gmail.com>
2022-10-26 12:42:33 -0700

    Updated hhow the program searches directories to be more efficient. The prgram will now extract all files/directories from all archives and recursively search those directories and extract additional archives

--- Log4J_Version_Check.py
+++ Log4J_Version_Check.py
@@ -1,55 +1,66 @@
 import os
+import re
 import sys
+import shutil
 import configparser
 from zipfile import ZipFile
 
-results = open("Log4J_Search_Results.txt", "wt")
-
-#Searches the directory given as the parameter.
-for root, dirs, files in os.walk(sys.argv[1]):
-	for name in files:
+def checkLog4J(filepath, results = ""):
+	for root, dirs, files in os.walk(filepath):
+		for name in files:
+			name.lower()
+			if name.startswith("log4j") and name.endswith("jar"):
+				print(results)
+				
+				#Extracting the manifest file from the JAR archive
+				zipper = ZipFile(root + "\\" + name, "r")
+				zipper.extract("META-INF/MANIFEST.MF")
+				
+				#Places a header for the ConfigParser
+				file = open("META-INF/MANIFEST.MF", "r")
+				temp = file.readlines()
+				temp.insert(0, "[Deafult]\n")
+				file.close()
+				
+				#Write the changes to the file.
+				file = open("META-INF/MANIFEST.MF", "wt")
+				for line in temp:
+					file.write(line)
+				file.close()
+				
+				#Uses a configuration parser to read the modified file and parse the version.
+				#WARNING: This part is unstable because the parser is not following enforced standards due to the 'strict=False' parameter.
+				manifestParser = configparser.ConfigParser(delimiters=':',strict=False)
+				manifestParser.read_file(open("META-INF/MANIFEST.MF", "rt"))
+				
+				#Adds finding to total results
+				results += "File found at: " + root + "\\" + name + "\n"
+				results += "Log4J Version is: " + manifestParser.get("Deafult","Implementation-Version") + "\n\n"
+				
+				shutil.rmtree("META-INF")
+			
+			elif re.search("jar$", name):
+				zipper = ZipFile(root + "\\" + name, "r")
+				zipDir = zipper.infolist()
+				
+				for i in zipDir:
+					if i.is_dir() and not i.filename == "META-INF":
+						zipper.extract(i.filename, "Log4JChecker_Temp")
+						results = checkLog4J("Log4JChecker_Temp\\" + i.filename, results)
+						shutil.rmtree("Log4JChecker_Temp\\" + i.filename)
+						
+						
+	return results
 	
-		#Puts the filename into lowercase for easier identification.
-		name.lower();
-		
-		#If the files starts with 'log4j' and ends with 'jar' proceed with version identification.
-		if name.startswith("log4j") and name.endswith("jar"):
-			filepath = root + "\\" + name
-			
-			#extracts the required manifest file from the JAR file.
-			zipper = ZipFile(filepath, "r")
-			zipper.extract("META-INF/MANIFEST.MF")
-			
-			#Reads the contents of a file to a list and insert a section header at the beginning.
-			file = open("META-INF/MANIFEST.MF", "r")
-			temp = file.readlines()
-			temp.insert(0, "[Deafult]\n")
-			file.close()
-			
-			#Write the changes to the file.
-			file = open("META-INF/MANIFEST.MF", "wt")
-			for line in temp:
-				file.write(line)
-			file.close()
-			
-			#Uses a configuration parser to read the modified file and parse the version.
-			#WARNING: This part is unstable because the parser is not following enforced standards due to the 'strict=False' parameter.
-			manifestParser = configparser.ConfigParser(delimiters=':',strict=False)
-			manifestParser.read_file(open("META-INF/MANIFEST.MF", "rt"))
-			
-			#Prints to standard out
-			print("File found at: " + filepath)
-			print("Log4J Version is: " + manifestParser.get("Deafult","Implementation-Version") + "\n\n")
-			
-			#Writes results to file.
-			results.write("File found at: " + filepath + "\n")
-			results.write("Log4J Version is: " + manifestParser.get("Deafult","Implementation-Version") + "\n\n")
-			
-			#Cleans up after itself.
-			os.remove("META-INF/MANIFEST.MF")
-			os.removedirs("META-INF")
+	
+#####Actual Program Start#####	
+if not os.path.exists("Log4JChecker_Temp"):
+	os.mkdir("Log4JChecker_Temp")
+	
+file = open("Log4J_Search_Results.txt", "wt")
+file.write(checkLog4J(sys.argv[1]))
 
-#Closes the results file cleanly.			
-results.close()
+if os.path.exists("Log4JChecker_Temp"):
+	shutil.rmtree("Log4JChecker_Temp")
 
-exit(0)
+file.close()
